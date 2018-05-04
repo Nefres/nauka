a9a62536cb0e4e548880c00e9884d3ba508dbb43
Nefres <38902894+Nefres@users.noreply.github.com>
2018-05-04 08:23:00 +0200

    Add files via upload

---
+++ pole i obwod kwadrat.py
@@ -0,0 +1,8 @@
+wysokosc = int(input('podaj wysokosc prostokata '))
+szerokosc = float(input('podaj prostokata '))
+
+pole = wysokosc * szerokosc
+obwod = 2 * (wysokosc + szerokosc)
+
+print('pole wynosi:', pole)
+print ('obwod wynosi', obwod)
